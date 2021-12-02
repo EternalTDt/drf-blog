c4f26158f64d7b2aed7b4d17e1edfcd84d27bf06
Tim <demidovbest@gmail.com>
2021-12-02 22:24:29 +0300

    added post app/ models, serializers, urls, permissions

--- api/views.py
+++ api/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics
-from .serializers import UserSerializer
+from rest_framework import generics, permissions
+from .serializers import UserSerializer, PostSerializer
 from django.contrib.auth.models import User
+from .permissions import IsOwnerOrReadOnly
+from .models import Post
 
 
 class UserList(generics.ListAPIView):
@@ -11,3 +13,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
